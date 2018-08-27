7f16be16181ce36da683534aa810691504300a40
imtyM <m.imtiazmukadam@gmail.com>
2018-08-27 12:09:33 +0200

    Implementing ml

---
+++ keras_regression.py
@@ -0,0 +1,23 @@
+import numpy
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+
+
+
+
+# define base model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal'))
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	return model
--- main.py
+++ main.py
@@ -7,4 +7,5 @@ myDB = mongoDB()
 
 # myDevice.debug_loop()
 fingerprint = myDevice.deep_scan_location(20)
+# fingerprint = myDevice.short_scan_fingerprint(5)
 myDB.updateOne(fingerprint)
