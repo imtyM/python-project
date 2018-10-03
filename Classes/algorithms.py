e385934f4ac7e1cb602a03c0f6e077456bba02da
imtyM <m.imtiazmukadam@gmail.com>
2018-10-03 11:09:12 +0200

    optomize localization

--- Classes/algorithms.py
+++ Classes/algorithms.py
@@ -1,4 +1,7 @@
 import os
+import numpy as np
+import itertools as it
+from collections import Counter
 
 KNOWN_CELLS_LAB = ['ef39', 'f4b2', 'da36', 'ea75', '35fd', '51a6', '51a5', 'd9dd', 'ef61', '4de6', 'bbd2', '471a', '403a', '4de7', '4de5', '4db5', 'cbb7', '46f2', 'e77f', '46df', '46dd', '51a7', '4db3', '4648', '3a68', 'cbb9', '4dc8', 'ccf7']
 class algorithms:
@@ -7,25 +10,70 @@ class algorithms:
         self.db = DBinstance
         self.primaryString = primary_string
 
-    def processQuery(self, inputData, deviationStep = 0.5):
+    def processQuery(self, inputData, deviationStep = 0.25):
         doc_count = 0
         cursor = None
         deviation = 0
+        found_locations = []
+
+
 
         cells = self._getCellsList(inputData)
+        cell_permutations = self._getPermutations(cells)
+        print('CELL PERMUTATIONS ', cell_permutations)
 
-        while doc_count == 0 and deviation < 20 : 
-            query = None
-            cursor = None
+        while doc_count == 0 and deviation < 10 : 
+            
+            for cellList in cell_permutations:
+                print('CURRENT CELL LIST: ', cellList)
 
-            query = self._buildQuery(cells,inputData, deviation)
-            cursor = self.db.findByQuery(query)
+                query = self._buildQuery(cellList, inputData, deviation)
+                cursor = self.db.findByQuery(query)
+                doc_count = cursor.count()
 
+                if doc_count > 0:
+                    # if we get a really strong match, break and accept it. 
+                    if len(cellList) > 3 and deviation < 4:
+                        print('BREAKING with deviation of ', deviation)
+                        print('Location of ', cursor.next()["location"]) 
+                        break
+                    for doc in cursor:
+                        found_locations.append(doc["location"])
 
-            doc_count = cursor.count()
             deviation += deviationStep
 
-        self._processResults(doc_count, deviation, cursor)
+        print('found locations ', found_locations)
+        print('most common location: ', self._mostCommon( found_locations ))
+        # self._processResults(doc_count, deviation, cursor)
+
+    def _mostCommon(self, locs):
+        if len(locs) > 0:
+            el = Counter(locs)
+            return el.most_common(1)[0][0]
+        return "none"
+
+    def _getPermutations(self, cells):
+        permutations = []
+        permutations.append(cells)
+        
+        if len(cells) > 6 :
+            for cellList in it.combinations(cells, 6):
+                permutations.append(list( cellList ))
+
+        if len(cells) > 5 :
+            for cellList in it.combinations(cells, 5):
+                permutations.append(list( cellList ))
+
+        if len(cells) > 4 :
+            for cellList in it.combinations(cells, 4):
+                permutations.append(list( cellList ))
+
+        if len(cells) > 3:
+            for cellList in it.combinations(cells, 3):
+                permutations.append(list( cellList ))
+
+        return permutations
+        
 
     def _getCellsList(self, inputData):
         cells = []
@@ -48,9 +96,6 @@ class algorithms:
         primaryString = self.primaryString
         for cell in cells:
             query[primaryString + cell] = {"$gt" : input_data[cell] - deviation, "$lte": input_data[cell] + deviation}
-        
-        print('Built query : \n', query, '\n')
-
         return query
 
 
