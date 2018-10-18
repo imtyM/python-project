c2eabf0206c77a3ac73105f78dd10400891c7c0c
imtyM <m.imtiazmukadam@gmail.com>
2018-10-18 13:56:28 +0200

    Added location post

--- localisation.py
+++ localisation.py
@@ -3,7 +3,8 @@ from Classes.mongoDB import mongoDB
 from serial.tools import list_ports
 from Classes.algorithms import algorithms
 import os
-
+import requests
+#  https://8pi5e8eqs7.execute-api.eu-west-1.amazonaws.com/latest
 myDevice = serialClass('/dev/ttyUSB0')
 myDB = mongoDB('lab')
 processor = algorithms(myDB)
@@ -15,5 +16,8 @@ while True:
 
     found_location = processor.debugProcess(fingerprint)
     print('THIS IS THE FOUND LOCATION: ', found_location, '\n\n\n\n\n')
+    print('POSTING REQ ------\n\n')
+    r = requests.post(' https://8pi5e8eqs7.execute-api.eu-west-1.amazonaws.com/latest/currentLocation', data={'currentLocation':found_location})
+    print('POST STATUS: ', r.status_code, r.reason)
 
 
