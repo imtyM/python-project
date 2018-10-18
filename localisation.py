818985b4fa5eb8576eb1589d4c06d94c8c136bdb
imtyM <m.imtiazmukadam@gmail.com>
2018-10-18 15:51:47 +0200

    bug fixes

--- localisation.py
+++ localisation.py
@@ -4,6 +4,7 @@ from serial.tools import list_ports
 from Classes.algorithms import algorithms
 import os
 import requests
+import json
 #  https://8pi5e8eqs7.execute-api.eu-west-1.amazonaws.com/latest
 myDevice = serialClass('/dev/ttyUSB0')
 myDB = mongoDB('lab')
@@ -17,7 +18,8 @@ while True:
     found_location = processor.debugProcess(fingerprint)
     print('THIS IS THE FOUND LOCATION: ', found_location, '\n\n\n\n\n')
     print('POSTING REQ ------\n\n')
-    r = requests.post('https://8pi5e8eqs7.execute-api.eu-west-1.amazonaws.com/latest/currentLocation', data={'currentLocation':found_location})
+    post_data = {"currentLocation":found_location}
+    r = requests.post('https://8pi5e8eqs7.execute-api.eu-west-1.amazonaws.com/latest/currentLocation', data=json.dumps(post_data))
     print('POST STATUS: ', r.status_code, r.reason)
 
 
